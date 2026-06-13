d3f1e0ddf559390b52d7a97521cc115a519404ad
Prathmesh Kharwade <prathmeshkharwade23@gmail.com>
2026-06-14 03:41:07 +0530

    Add initial React setup with Vite, including SVG assets and main entry point
    - Created main.jsx as the entry point for the React application.
    - Added index.css for global styles.
    - Introduced react.svg and vite.svg assets for branding.
    - Configured Vite with react plugin in vite.config.js.

---
+++ Gsap/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="box">7</div>
+    </div>
+  )
+}
+
+export default App
--- day3/src/main.js
+++ day3/src/main.js
@@ -3,11 +3,12 @@ import { Draggable } from "gsap/Draggable";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
 import { InertiaPlugin } from "gsap/InertiaPlugin";
+import { Flip } from "gsap/Flip";
 
 
 import "./style.css";
 
-gsap.registerPlugin(ScrollTrigger, SplitText, Draggable , InertiaPlugin );
+gsap.registerPlugin(ScrollTrigger, SplitText, Draggable , InertiaPlugin ,Flip);
 
 
 // const split = new SplitText('.title h1', {
@@ -28,10 +29,38 @@ gsap.registerPlugin(ScrollTrigger, SplitText, Draggable , InertiaPlugin );
 
 
 
-Draggable.create(".box" , {
-  bounds: "#app",
-  edgeResistance: 1,
-  inertia : true,
-  dragResistance: 0.2,
+// Draggable.create(".box" , {
+//   bounds: "#app",
+//   edgeResistance: 1,
+//   inertia : true,
+//   dragResistance: 0.2,
 
+// });
+
+const img = document.querySelector(".specialImage");
+const img2 = document.querySelector(".specialImage2");
+
+
+
+img.addEventListener('click', ()=> {
+
+    const state = Flip.getState(img);
+    const state2 = Flip.getState(img2);
+
+
+    document.querySelector(".imageGallery").appendChild(img2);
+    document.querySelector(".imageShow").appendChild(img);
+
+    Flip.from(state, {
+        duration: 1.3,
+        ease: "power3.inOut",
+        absolute: true,
+        scale: true,
+    });
+    Flip.from(state2, {
+        duration: 1.3,
+        ease: "power3.inOut",
+        absolute: true,
+        scale: true,
+    });
 });
